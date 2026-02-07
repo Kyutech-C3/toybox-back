4de2ed68919682014922ad1faea14f99e2627e19
Taku2515 <it.program.2357@gmail.com>
2026-02-07 16:33:05 +0900

    [docs] #31 S3キー抽出箇所にコメント追加

--- internal/infrastructure/database/asset/repository.go
+++ internal/infrastructure/database/asset/repository.go
@@ -163,6 +163,8 @@ func (r *AssetRepository) UploadAvatar(ctx context.Context, discordUserID string
 }
 
 func (r *AssetRepository) DeleteFile(ctx context.Context, url string) error {
+	// S3の完全なURLからS3オブジェクトのキーを抽出する。
+	// 例: "https://s3.REGION.amazonaws.com/BUCKET_NAME/path/to/object" から "path/to/object" を抽出
 	prefix := config.S3_BASE_URL + "/" + config.S3_BUCKET + "/"
 	key := ""
 	if len(url) > len(prefix) && url[:len(prefix)] == prefix {
